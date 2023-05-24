239b3c0251f978c696ea23d5533002fa30265e91
Kalesha-Sk <kalesha.shaik@prasthana.com>
2023-05-24 15:35:27 +0530

    conference updates

--- src/app/conference/conference.component.ts
+++ src/app/conference/conference.component.ts
@@ -9,19 +9,21 @@ import { of } from 'rxjs';
   styleUrls: ['./conference.component.scss']
 })
 export class ConferenceComponent {
-  private authId: string = '';
-  private authToken: string = '';
+  private authId: string = 'MAYWVLMZBMZDJKYZBKYT';
+  private authToken: string = 'YzhjYmE4MGYzZDhmN2JjNTMyMmE1OTFlZWE2MWM3';
 
   public from: string = '';
   public to: string = '';
   public callInProgress: boolean = false;
   public callComplete: boolean = false;
   public errorMessage: string = '';
+  public baseUrl: string = 'https://api.plivo.com/v1/Account/';
 
   constructor(private http: HttpClient) { }
 
   makeCall() {
     this.callInProgress = true;
+
     const headers = new HttpHeaders({
       Authorization: `Basic ${btoa(`${this.authId}:${this.authToken}`)}`,
       'Content-Type': 'application/json',
@@ -32,9 +34,17 @@ export class ConferenceComponent {
     const body = {
       from: this.from,
       to: this.to,
+      app_name: 'ConferenceCall',
+      answer_url: 'https://kalesha-sk.github.io/xmldoc/ConferenceXml.xml',
+      answer_method: 'GET',
+      hangup_url: 'https://s3.amazonaws.com/plivosamplexml/hangup_url.xml',
+      hangup_method: 'GET',
+      fallback_url: 'https://s3.amazonaws.com/plivosamplexml/fallback_url.xml',
+      fallback_method: 'GET',
     };
 
-    this.http.post('', body, options)
+    this.http
+      .post(this.baseUrl + this.authId + '/Call', body, options)
       .pipe(
         catchError((err) => {
           this.errorMessage = `Error making call: ${err.message}`;
@@ -47,6 +57,18 @@ export class ConferenceComponent {
           this.callComplete = true;
         }
       });
+
+    this.http
+      .get('https://kalesha-sk.github.io/xmldoc/ConferenceXml.xml')
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.error(error);
+        }
+    );
+
   }
 
 }
